e0ddf4a990c39d96da1cb0b885b33f4791730382
CLAIRE LEVY <clairel@uw.edu>
2015-05-08 19:11:24 -0700

    update but still not the right thing

--- Malderelli annotations script.R
+++ Malderelli annotations script.R
@@ -16,6 +16,7 @@ GeneSymbols<-as.character(MalderelliData$Gene) #gene symbols
 
 #set up mart
 ensembl<-useMart("ensembl",dataset="hsapiens_gene_ensembl")
+save(ensembl, file = "ensemblMart08May15")
 
 #get entrez id, go id, strand, go name and go category
 MalderelliAnnotations<-getBM(attributes=c("hgnc_symbol","entrezgene",
@@ -23,8 +24,24 @@ MalderelliAnnotations<-getBM(attributes=c("hgnc_symbol","entrezgene",
                    mart = ensembl, values=GeneSymbols,
                    filters="hgnc_symbol")
 
+#BP only, then take out the BP column
+
+BPMalderelliAnnotations<-MalderelliAnnotations%>%
+  filter(namespace_1003=="biological_process")%>%
+  select(-namespace_1003)
+
+#eliminating any rows with  missing goid or symbol(none)
+BPMalderelliAnnotations <- BPMalderelliAnnotations[
+  nchar(BPMalderelliAnnotations$hgnc_symbol)>0 &
+    nchar(BPMalderelliAnnotations$go_id)>0,]
 
 
-save(MalderelliAnnotations, file="MalderelliAnnotations_7May15.Rda")
+save(BPMalderelliAnnotations, file="BPMalderelliAnnotations_08May15.Rda")
+
+Short_BPMalderelliAnnotations = unique(BPMalderelliAnnotations[,c(1,3)])
+
+BPMalderelliGoAndSymbolList<-unstack(Short_BPMalderelliAnnotations)
+
+save(BPMalderelliGoAndSymbolList, file = "BPMalderelliGoAndSymbolList_08May15.Rda")
 
 
