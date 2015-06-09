0f7320b109bcc1498a6506f09454893fe6f3d1cf
CLAIRE LEVY <clairel@uw.edu>
2015-06-09 14:13:23 -0700

    changed method to convert list to df plus some tidying

--- Malderelli annotations script.R
+++ Malderelli annotations script.R
@@ -2,7 +2,8 @@
 ##################Getting new go terms for Malderelli data ################
 
 require(biomaRt)
-
+require(dplyr)
+require(stringr)
 setwd("J:/MacLabUsers/Claire/Projects/HIV-integration")
 
 #load data spreadsheet
@@ -11,20 +12,23 @@ load("MalderelliData.formatted.likeSCRIData.Rda")
 #give the data frame an easier name
 MalderelliData<-MalderelliData.formatted.likeSCRIData
 
+Genes<-unique(as.character(MalderelliData$Gene))
 
 #set up mart
 
+load("ensemblMart11May15.Rda")
 ensembl<-useMart("ensembl",dataset="hsapiens_gene_ensembl")
 
 
-save(ensembl, file = "ensemblMart11May15")
 
 
 #get entrez id, go id, strand, go name and go category
 MalderelliAnnotations<-getBM(attributes=c("hgnc_symbol",
                                 "go_id","namespace_1003"),
-                   mart = ensembl, values=MalderelliData$Gene,
+                   mart = ensembl, values=Genes,
                    filters="hgnc_symbol")
+
+load("MalderelliAnnotations8June15.Rda")
 
 #BP only, then take out the BP column
 
@@ -37,52 +41,33 @@ BPMalderelliAnnotations <- BPMalderelliAnnotations[
   nchar(BPMalderelliAnnotations$hgnc_symbol)>0 &
     nchar(BPMalderelliAnnotations$go_id)>0,]
 
+#make sure entries are unique
+UniqueBPMalderelliAnnotations<-unique(BPMalderelliAnnotations)
+#same # of observations as the non-unique version
+
 
 #convert the annotation df (with repeated symbols)to a list of go ids
 # and the symbols that are in them
 
 BPMalderelliGoAndSymbolList<-unstack(BPMalderelliAnnotations)
 
-#what is the length of each element (how many genes in each go id)
-x<-sapply(BPMalderelliGoAndSymbolList,FUN=length)
-#how many have <1 gene in them?
-sum(x<1) # none
-
-LogicalBPMalderelliGOAndSymbolList<- lapply(BPMalderelliGoAndSymbolList,
-                                  FUN=function(i)factor(as.integer(MalderelliData$Gene %in% i)))
 
 
+## For each gene symbol that I put into biomaRt (Genes),
+# is that symbol present or absent from each named element in GOAndSymbolList?
 
-#http://stackoverflow.com/questions/18747800/fast-way-of-converting-large-list-to-dataframe
-#make a data frame out of the list with one column per go id and symbols as
-#rownames. values are 0's and 1's
-
-n<-length(LogicalBPMalderelliGOAndSymbolList[[1]])
-
-dfBPMalderelliGOAndSymbol<-structure(LogicalBPMalderelliGOAndSymbolList,
-                                     row.names = c(NA, -n), class = "data.frame")
-
-#change colons to periods
-colnames(dfBPMalderelliGOAndSymbol)<-str_replace_all(colnames(dfBPMalderelliGOAndSymbol),":","\\.")
+LogicalBPMalderelliGOAndSymbolList<-lapply(BPMalderelliGoAndSymbolList,
+          FUN=function(i)factor(as.integer(MalderelliData$Gene %in% i)))
 
 
+#make that list into a data frame with GO ids as the columns
+
+dfBPMalderelliGOAndSymbol<-data.frame(LogicalBPMalderelliGOAndSymbolList)
+
+
+
+#combine with other ID columns from original data set
 CLMalderelliData.formatted.likeSCRIData<-cbind(MalderelliData[,1:8],dfBPMalderelliGOAndSymbol)
-
-###################   SAVE ####################################
-
-
-save(CLMalderelliData.formatted.likeSCRIData,
-     file = "CLMalderelliData.formatted.likeSCRIData.Rda")
-
-
-
-
-
-
-
-
-
-
 
 
 #look back in annotation list to check that things are aligned 
@@ -92,9 +77,35 @@ head(BPMalderelliGoAndSymbolList)
 #so in rows RAB1A and ULK2, column GO:00000045 should !=0 since
 #I know from BPMalderelliGOAndSymbolList that both symbols are in that
 #go id
-CLMalderelliData[CLMalderelliData$"GO:0000045"!=0,c("Gene","GO:0000045")]
+CLMalderelliData.formatted.likeSCRIData[CLMalderelliData.formatted.likeSCRIData$"GO.0000045"!=0,c("Gene","GO.0000045")]
 
-CLMalderelliData[CLMalderelliData$Gene=="RAB1A","GO:0000045"]
+CLMalderelliData.formatted.likeSCRIData[CLMalderelliData.formatted.likeSCRIData$Gene=="RAB1A","GO.0000045"]
 
 
 
+
+
+
+################################   SAVE ####################################
+
+
+save(CLMalderelliData.formatted.likeSCRIData,
+     file = "CLMalderelliData.formatted.likeSCRIData.Rda")
+
+
+
+
+######################## Filtering for Hallmark terms ##############
+
+hallmark<-read.csv("hallmarksCancer_GO_CL.csv")
+hallmark$go_id<-as.character(hallmark$go_id)
+
+c<-unique(BPMalderelliAnnotations$go_id %in% hallmark$go_id)
+
+#filter out just the hallmark go_ids that overlapped with those that I
+#already found
+
+hallmarkFiltered<-dfBPMalderelliGOAndSymbol[,c]
+
+
+CLMalderelliHallmarkFiltered<-cbind(MalderelliData[,1:8],hallmarkFiltered)
