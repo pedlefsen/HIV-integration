faadd83aecb44e7d6577806b92968c37ca4583a8
Paul <pedlefse@scharp.org>
2015-09-21 11:36:44 -0700

    Fixes how the GO annotations are done for the Malderelli data; they had not matched how they were done for the SCRI data, but now they should.

--- Malderelli annotations script.R
+++ Malderelli annotations script.R
@@ -1,5 +1,6 @@
-
 ##################Getting new go terms for Malderelli data ################
+## Writen by Claire Levy
+## Modified by Paul E on September 9, 2015
 
 require(biomaRt)
 require(dplyr)
@@ -7,125 +8,102 @@ require(stringr)
 require(HGNChelper)
 require(reshape2)
 
-setwd("J:/MacLabUsers/Claire/Projects/HIV-integration")
-
 #load data spreadsheet
-load("MalderelliData.formatted.likeSCRIData.Rda")
+load( "MalderelliData.formatted.likeSCRIData.Rda" );
 
-#here are the terms that I want to check against the annotations
-#that I get for the Malderelli list
-termsToCheck<-colnames(MalderelliData.formatted.likeSCRIData[,9:48])
-
-#using HGNChelper to check symbols and provide corrections
-hgncCheck<-checkGeneSymbols(MalderelliData.formatted.likeSCRIData$Gene,
-                    unmapped.as.na = FALSE)
-
+######
+#using HGNChelper to check gene symbols and provide corrections
+######
+hgncCheck <- checkGeneSymbols( MalderelliData.formatted.likeSCRIData$Gene,
+                              unmapped.as.na = TRUE );
 #if hgnc help can't find a good symbol, it can leave as NA or leave
 #the input if unmapped.as.na=FALSE
 
-
-
-MalderelliData.formatted.likeSCRIData<-cbind(hgncCheck,MalderelliData.formatted.likeSCRIData)
-
-#check what the wrong ones look like
-y<-filter(hgncCheck,Approved==FALSE)
-w<-unique(y)
+## These didn't map:
+# > hgncCheck[ is.na( hgncCheck[ , "Suggested.Symbol" ] ), ]
+#                  x Approved Suggested.Symbol
+# 209      LOC283050    FALSE             <NA>
+# 210      LOC283050    FALSE             <NA>
+# 316      LOC643733    FALSE             <NA>
+# 324   LOC100526771    FALSE             <NA>
+# 325   LOC100526771    FALSE             <NA>
+# 578        HDGFRP3    FALSE             <NA>
+# 949   LOC100294362    FALSE             <NA>
+# 1099  LOC100506012    FALSE             <NA>
+# 1257  LOC100505746    FALSE             <NA>
+# 1620  LOC100294145    FALSE             <NA>
+# 1697     LOC729852    FALSE             <NA>
+# 1698     LOC729852    FALSE             <NA>
+# 1774 GIMAP1-GIMAP5    FALSE             <NA>
+# 1775 GIMAP1-GIMAP5    FALSE             <NA>
+# 1777        SGK223    FALSE             <NA>
+# 1778        SGK223    FALSE             <NA>
 
 #keep the correct names
-MalderelliData.formatted.likeSCRIData<-MalderelliData.formatted.likeSCRIData[,-c(1:2,6)]
-#change the col name
-names(MalderelliData.formatted.likeSCRIData)[1]<-"Gene"
-#fix this weird one
-MalderelliData.formatted.likeSCRIData$Gene<-str_replace(MalderelliData.formatted.likeSCRIData$Gene,
-                                                        "MARC2 /// MARCH2","MARCH2")
+MalderelliData.formatted.likeSCRIData.fixed <- MalderelliData.formatted.likeSCRIData;
+MalderelliData.formatted.likeSCRIData.fixed$Gene <- hgncCheck$Suggested.Symbol;
+
+# fix this weird one
+MalderelliData.formatted.likeSCRIData.fixed$Gene[ grep( "///", MalderelliData.formatted.likeSCRIData.fixed$Gene ) ] <- "MARCH2";
+
 #new gene list for mart has all correct symbols
-Genes<-unique(MalderelliData.formatted.likeSCRIData$Gene)
+unique.genes <- unique( MalderelliData.formatted.likeSCRIData.fixed$Gene );
 
-
-
+####
+# Mapping GO terms
+####
+#here are the terms that I want to check against the annotations
+#that I get for the Malderelli list
+terms.we.care.about <-
+    grep( "^GO", colnames( MalderelliData.formatted.likeSCRIData ), value = T );
 
 #set up mart
 
-load("ensemblMart11May15.Rda")
-# ensembl<-useMart("ensembl",dataset="hsapiens_gene_ensembl")
+load( "ensemblMart11May15.Rda" );
+ensembl<-useMart("ensembl",dataset="hsapiens_gene_ensembl")
 # 
 # #get entrez id, go id, strand, go name and go category
-# MalderelliAnnotations<-getBM(attributes=c("hgnc_symbol",
-#                                 "go_id","namespace_1003"),
-#                    mart = ensembl, values=Genes,
-#                    filters="hgnc_symbol")
+# unique.genes.GO.annotations <-
+#     getBM( attributes=c(
+#                "hgnc_symbol",
+#                "go_id",
+#                "namespace_1003"),
+#           mart = ensembl,
+#           values = unique.genes,
+#           filters="hgnc_symbol" );
+# save( unique.genes.GO.annotations, file = "MalderelliAnnotations9Sept15.Rda" );
+load( file = "MalderelliAnnotations9Sept15.Rda" );
 
-load("MalderelliAnnotations3Sept15.Rda")
+# BP only, and take out the extra column
+unique.genes.GO.annotations.BP <-
+    unique.genes.GO.annotations[
+      unique.genes.GO.annotations$namespace == "biological_process", 1:2
+    ];
 
-
-#BP only, then take out the "BP" column
-
-BPMalderelliAnnotations<-MalderelliAnnotations%>%
-  filter(namespace_1003=="biological_process")%>%
-  select(-namespace_1003)
-
-#eliminating any rows with  missing goid or symbol(none)
-BPMalderelliAnnotations <- BPMalderelliAnnotations[
-  nchar(BPMalderelliAnnotations$hgnc_symbol)>0 &
-    nchar(BPMalderelliAnnotations$go_id)>0,]
-
-#make sure entries are unique
-UniqueBPMalderelliAnnotations<-unique(BPMalderelliAnnotations)
-#same # of observations as the non-unique version
-
-#replace the colon in "GO:xxxxxxx" with a period so it matches
-#the formatting in the SCRI data
-
-UniqueBPMalderelliAnnotations$go_id<-str_replace(UniqueBPMalderelliAnnotations$go_id,
-                                                 ":","\\.")
-
-#convert the annotation df (with repeated symbols)to a list of gene symbols symbols
-# and the go ids annotated to them.
-
-#the "form" argument says which variable should be the sub elements
-#of the list and which should be the names of the elements.
-
-SymbolTermList<-unstack(UniqueBPMalderelliAnnotations, form = go_id~hgnc_symbol)
-
-
-
-#Look at each gene symbol in the list and put a 1 if that element
-#contains a go term from termsToCheck and 0 if it doesn't.
-
-TermCheckList<-lapply(SymbolTermList,
-          FUN=function(i)factor(as.integer(termsToCheck %in% i)))
-
-
-#turn the list into a data frame (makes symbols the columns)
-#then transpose the rows and columns using t()
-df<-t(data.frame(TermCheckList))
-
-
-#make the go ids the col names
-colnames(df)<-termsToCheck
-
-#it got turned into a matrix so change back to df
-df<-as.data.frame(df)
-
-#make the gene symbols a real column instead of row names
-#and put it first
-df<-df%>%
-  mutate("Gene"=rownames(df))%>%
-  select(Gene,1:40)
-
-#I could merge in the original info from the MalderelliData.formatted...
-#file but I don't trust merge to do it all correctly so I'm
-#just saving this
-
-
-save(df,file="MalderelliData BP only checked terms 9Sept15.Rda")
-
-
-symbolCheck<-cbind(unique(hgncCheck))
-colnames(symbolCheck)<-c("origNames","Approved","biomaRtInput")
-bioMartOut<-unique(MalderelliAnnotations$hgnc_symbol)
-biomaRtBPfiltered<-unique(BPMalderelliAnnotations$hgnc_symbol)
-
-save(symbolCheck,file="symbolCheck.Rda")
-save(bioMartOut,file="bioMartOutput.Rda")
-save(biomaRtBPfiltered, file="bioMartBPfilteredOutput.Rda")
+.go.terms.columns <- grep( "^GO", colnames( MalderelliData.formatted.likeSCRIData.fixed.further ) );
+MalderelliData.formatted.likeSCRIData.fixed.further <-
+    apply( MalderelliData.formatted.likeSCRIData.fixed, 1, function( .row ) {
+        .row[ .go.terms.columns ] <- 0;
+        .go.terms.for.row <- unique.genes.GO.annotations.BP[ unique.genes.GO.annotations.BP[ , "hgnc_symbol" ] == .row[ "Gene" ], "go_id" ];
+        # Replace ":" with "." for this.
+        .go.terms.for.row <- gsub( ":", ".", .go.terms.for.row );
+        if( length( .go.terms.for.row ) == 0 ) {
+            ## This means we don't know that they are or are not part
+            ## of a term, but since these terms are collections of
+            ## what we know and omit many things we do not know, we
+            ## let 0 code for "don't know" and leave things as they
+            ## are.
+        } else {
+            # Further filter it.
+            .go.terms.for.row.among.those.we.care.about <-
+                setdiff( terms.we.care.about, .go.terms.for.row );
+            if( length( .go.terms.for.row.among.those.we.care.about ) > 0 ) {
+                stopifnot( all( .go.terms.for.row.among.those.we.care.about %in% names( .row ) ) );
+                .row[ .go.terms.for.row.among.those.we.care.about ] <- 1;
+            }
+        }
+        return( .row );
+    } );
+MalderelliData.formatted.likeSCRIData.fixed <-
+    t( MalderelliData.formatted.likeSCRIData.fixed.further );
+save( MalderelliData.formatted.likeSCRIData.fixed, file = "MalderelliData.formatted.likeSCRIData.fixed.Rda" );
